65a693ace8380b13f3ea4e740874c9507777f398
ValentinaLV <tina.lysenok@gmail.com>
2020-01-19 22:01:56 +0200

    added scraping vacancies from rabota.ua and dou.ua

--- scraping/utils.py
+++ scraping/utils.py
@@ -1,42 +1,11 @@
 import time
+import datetime
 import requests
 from bs4 import BeautifulSoup as BS
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:47.0) Gecko/20100101 Firefox/47.0',
            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
            }
-
-
-def djinni():
-    session = requests.Session()
-
-    base_url = 'https://djinni.co/jobs/?primary_keyword=Python&location=%D0%9A%D0%B8%D0%B5%D0%B2'
-
-    domain = 'https://djinni.co'
-
-    jobs = []
-    urls = [base_url, base_url + '&page=2']
-
-    for url in urls:
-        time.sleep(2)
-        req = session.get(url, headers=headers)
-        if req.status_code == 200:
-            bsObj = BS(req.content, "html.parser")
-            li_list = bsObj.find_all('li', attrs={'class': 'list-jobs__item'})
-            for li in li_list:
-                div = li.find('div', attrs={'class': 'list-jobs__title'})
-                title = div.a.text
-                href = div.a['href']
-                short = 'No description'
-                # company = "No name"
-                descr = li.find('div', attrs={'class': 'list-jobs__description'})
-                if descr:
-                    short = descr.p.text
-                jobs.append({'href': domain + href,
-                             'title': title,
-                             'description': short,
-                             'company': "No name"})
-    return jobs
 
 
 def work():
@@ -74,4 +43,88 @@ def work():
                              'title': title.text,
                              'description': short,
                              'company': company})
+    return jobs
+
+
+def rabota():
+    session = requests.Session()
+
+    base_url = 'https://rabota.ua/jobsearch/vacancy_list?regionId=1&keyWords=python&period=2&lastdate='
+
+    domain = 'https://rabota.ua'
+    jobs = []
+    urls = []
+    yesterday = datetime.date.today() - datetime.timedelta(1)
+    one_day_ago = yesterday.strftime('%d.%m.%Y')
+    base_url = base_url + one_day_ago
+    urls.append(base_url)
+
+    req = session.get(base_url, headers=headers)
+    if req.status_code == 200:
+        bsObj = BS(req.content, "html.parser")
+        pagination = bsObj.find('dl', attrs={'id': 'content_vacancyList_gridList_pagerInnerTable'})
+        if pagination:
+            pages = pagination.find_all('a', attrs={'class': 'f-always-blue'})
+            for page in pages:
+                urls.append(domain + page['href'])
+
+    for url in urls:
+        time.sleep(2)
+        req = session.get(url, headers=headers)
+        if req.status_code == 200:
+            bsObj = BS(req.content, "html.parser")
+            table = bsObj.find('table', attrs={'id': 'content_vacancyList_gridList'})
+            if table:
+                tr_list = bsObj.find_all('tr', attrs={'id': True})
+                for tr in tr_list:
+                    h3 = tr.find('h3', attrs={'class': 'f-vacancylist-vacancytitle'})
+                    title = h3.a.text
+                    href = h3.a['href']
+                    short = 'No description'
+                    company = "No name"
+                    logo = tr.find('p', attrs={'class': 'f-vacancylist-companyname'})
+                    if logo:
+                        company = logo.a.text
+                    p = tr.find('p', attrs={'class': 'f-vacancylist-shortdescr'})
+                    if p:
+                        short = p.text
+                    jobs.append({'href': domain + href,
+                                 'title': title,
+                                 'description': short,
+                                 'company': company})
+    return jobs
+
+
+def dou():
+    session = requests.Session()
+
+    base_url = 'https://jobs.dou.ua/vacancies/?city=%D0%9A%D0%B8%D0%B5%D0%B2&category=Python'
+
+    jobs = []
+    urls = [base_url]
+
+    for url in urls:
+        time.sleep(2)
+        req = session.get(url, headers=headers)
+        if req.status_code == 200:
+            bsObj = BS(req.content, "html.parser")
+            div = bsObj.find('div', attrs={'id': 'vacancyListId'})
+            if div:
+                li_list = bsObj.find_all('li', attrs={'class': 'l-vacancy'})
+                for li in li_list:
+                    a = li.find('a', attrs={'class': 'vt'})
+                    title = a.text
+                    href = a['href']
+                    short = 'No description'
+                    company = "No name"
+                    a_company = li.find('a', attrs={'class': 'company'})
+                    if a_company:
+                        company = a_company.text
+                    descr = li.find('div', attrs={'class': 'sh-info'})
+                    if descr:
+                        short = descr.text
+                    jobs.append({'href': href,
+                                 'title': title,
+                                 'description': short,
+                                 'company': company})
     return jobs
--- scraping/views.py
+++ scraping/views.py
@@ -1,5 +1,6 @@
+from django.db import IntegrityError
 from django.shortcuts import render
-from .utils import djinni, work
+from .utils import work, rabota, dou
 from .models import City, Speciality, Vacancy
 
 
@@ -8,7 +9,10 @@ def search_job(request):
 
 
 def get_vacancies(request):
-    jobs = work()
+    jobs = []
+    jobs.extend(work())
+    jobs.extend(rabota())
+    jobs.extend(dou())
     city = City.objects.get(name='Киев')
     speciality = Speciality.objects.get(name='Python')
     vacancies = Vacancy.objects.filter(city=city.id, speciality=speciality.id).values('url')
@@ -22,8 +26,12 @@ def get_vacancies(request):
                               title=job['title'],
                               description=job['description'],
                               company=job['company'])
-            vacancy.save()
+            try:
+                vacancy.save()
+            except IntegrityError:
+                pass
 
     return render(request, 'vacancies.html', {
         'jobs': jobs
     })
+
