447bfaeeb3c0c38c747c9a8d3b6bcf7d20140f43
ValentinaLV <tina.lysenok@gmail.com>
2020-01-19 18:46:11 +0200

    create city, speciality and vacancy models

---
+++ scraping/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import City, Speciality, Vacancy
+
+admin.site.register(City)
+admin.site.register(Speciality)
+admin.site.register(Vacancy)
---
+++ scraping/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+class City(models.Model):
+    name = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'City'
+        verbose_name_plural = 'Cities'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Speciality(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Speciality'
+        verbose_name_plural = 'Specialities'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Vacancy(models.Model):
+    url = models.CharField(max_length=250, unique=True)
+    title = models.CharField(max_length=250)
+    description = models.TextField(blank=True)
+    company = models.CharField(max_length=100, blank=True)
+    city = models.ForeignKey(City, on_delete=models.CASCADE, blank=True)
+    speciality = models.ForeignKey(Speciality, on_delete=models.CASCADE, blank=True)
+    timestamp = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Vacancy'
+        verbose_name_plural = 'Vacancies'
+
+    def __str__(self):
+        return f'{self.title}'
