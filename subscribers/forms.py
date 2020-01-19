84f5686b790e26c81a11d76c872c357375b5b4f6
ValentinaLV <tina.lysenok@gmail.com>
2020-01-20 00:43:21 +0200

    implement form, view and html template for subscribe creation

---
+++ subscribers/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+
+from scraping.models import Speciality, City
+from subscribers.models import Subscriber
+
+
+class SubscriberModelForm(forms.ModelForm):
+    email = forms.EmailField(label='Email',
+                             required=True,
+                             widget=forms.EmailInput(
+                                 attrs={"class": 'form-control'}
+                             ))
+    city = forms.ModelChoiceField(label='City',
+                                  queryset=City.objects.all(),
+                                  widget=forms.Select(
+                                      attrs={'class': 'form-control'}
+                                  ))
+    speciality = forms.ModelChoiceField(label='Speciality',
+                                        queryset=Speciality.objects.all(),
+                                        widget=forms.Select(
+                                            attrs={'class': 'form-control'}
+                                        ))
+    password = forms.CharField(label='Password',
+                               widget=forms.PasswordInput(
+                                   attrs={'class': 'form-control'}
+                               ))
+    is_active = forms.BooleanField(label='I agree to receive the newsletter with vacancies on my email',
+                                   required=True,
+                                   widget=forms.CheckboxInput(
+                                       attrs={'class': 'form-check-input', 'checked': 'checked'}
+                                   ))
+
+    class Meta:
+        model = Subscriber
+        fields = ('email', 'city', 'specialty', 'password', 'is_active')
--- subscribers/views.py
+++ subscribers/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+from .forms import SubscriberModelForm
+from .models import Subscriber
+
+
+class SubscriberCreate(CreateView):
+    model = Subscriber
+    form_class = SubscriberModelForm
+    template_name = 'create.html'
+    success_url = reverse_lazy('create')
+
+    def post(self, request, *args, **kwargs):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        if form.is_valid():
+            messages.success(request, 'Data was saved successfully.')
+            return self.form_valid(form)
+        else:
+            messages.error(request, 'Check if the form was filled out correctly.')
+            return self.form_invalid(form)
